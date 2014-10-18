b375cdc3cab569fa0601a6dece5ca9e74d484595
Rico Huijbers <rix0rrr@gmail.com>
2014-10-18 14:15:22 +0200

    Force input to be parsed as numbers

---
+++ js/ko-helpers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+ko.extenders.numeric = function(target) {
+    //create a writable computed observable to intercept writes to our observable
+    var result = ko.pureComputed({
+        read: target,  //always return the original observables value
+        write: function(newValue) {
+            var current = target(),
+                newValueAsNum = isNaN(newValue) ? 0 : parseFloat(+newValue);
+ 
+            //only write if it changed
+            if (newValueAsNum !== current) {
+                target(newValueAsNum);
+            } 
+        }
+    }).extend({ notify: 'always' });
+ 
+    return result;
+};
